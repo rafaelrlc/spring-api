7759009d1c7e0ed476514c3ebbc57538ded9e464
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-03-07 01:49:16 -0300

    first commit

---
+++ vendas/src/main/java/com/rafaelrlc/vendas/VendasApplication.java
@@ -0,0 +1,20 @@
+package com.rafaelrlc.vendas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class VendasApplication {
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "HELLO";
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
+    }
+
+}
