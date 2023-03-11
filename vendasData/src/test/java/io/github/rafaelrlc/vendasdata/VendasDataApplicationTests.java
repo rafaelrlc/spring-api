39c3f14b37ce11dda9a84dec048855d1c155ad88
Rafael Ribeiro <105018495+rafaelrlc@users.noreply.github.com>
2023-03-11 15:06:01 -0300

    update

---
+++ vendasData/src/test/java/io/github/rafaelrlc/vendasdata/VendasDataApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rafaelrlc.vendasdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
